aab69e358aa39bb9a2df0987472ea536d8045b7d
Kien Pham <kien@kienpham.com>
2021-01-24 09:46:16 -0800

    support listing

--- api/api/db.py
+++ api/api/db.py
@@ -1,12 +1,15 @@
 import boto3
+from boto3.dynamodb.conditions import Key
 
 ddb = boto3.resource('dynamodb')
+
 
 class DDB:
     def __init__(self, table_name="pro-licenses", partition_key="state-agency", sort_key="license-number"):
         self.table = ddb.Table(table_name)
         self.partition_key = partition_key
         self.sort_key = sort_key
+        self.query_limit = 50
 
     def get_state_agency_license(self, state, agency, license_number):
         partition_key = "{}-{}".format(state, agency)
@@ -25,8 +28,9 @@ class DDB:
     def get_state_agency(self, state, agency):
         partition_key = "{}-{}".format(state, agency)
 
-        res = self.table.get_item(Key={
-            self.partition_key: partition_key,
-        })
+        res = self.table.query(
+            Limit=self.query_limit,
+            KeyConditionExpression=Key(self.partition_key).eq(partition_key)
+        )
 
-        return res
+        return res['Items']
